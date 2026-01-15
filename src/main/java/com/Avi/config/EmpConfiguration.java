d991f1c858439e337b1e47dd8d5388d5aedcf23b
AvyaGupta-14 <avyagupta7@gmail.com>
2026-01-16 01:36:23 +0530

    Initial commit

---
+++ src/main/java/com/Avi/config/EmpConfiguration.java
@@ -0,0 +1,36 @@
+package com.Avi.config;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+//import com.mysql.cj.xdevapi.SessionFactory;
+
+public class EmpConfiguration {
+	
+	public static SessionFactory getSessionFactory()
+	{
+		Properties properties = new Properties();
+		
+		properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+		properties.put(Environment.JAKARTA_JDBC_URL, "jdbc:mysql://localhost:3306/hibernate");
+		properties.put(Environment.JAKARTA_JDBC_USER, "root");
+		properties.put(Environment.JAKARTA_JDBC_PASSWORD, "Avya@14");
+		properties.put(Environment.HBM2DDL_AUTO, "create");
+		properties.put(Environment.SHOW_SQL, "true");
+		properties.put(Environment.FORMAT_SQL, "true");
+		
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(properties).build();
+		Metadata metaData = new MetadataSources(ssr).addAnnotatedClass(com.Avi.entity.Employee.class).getMetadataBuilder().build();
+		SessionFactory sessionFactory = metaData.buildSessionFactory();
+
+
+		return sessionFactory;
+	}
+
+}
---
+++ src/main/java/com/Avi/main/EmployeeRunner.java
@@ -0,0 +1,49 @@
+package com.Avi.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import com.Avi.entity.Employee;
+
+public class EmployeeRunner {
+
+	public static void main(String[] args) {
+		
+		
+		Employee emp = new Employee(8, "Renu", "Female", 64548);
+	//	Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+	//	SessionFactory sessionFactory = cfg.buildSessionFactory();
+	//	SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		
+		//way1
+	//	StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+	//	Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+//		SessionFactory sessionFactory = metadata.buildSessionFactory();
+		
+		//way2
+		SessionFactory sessionFactory = new MetadataSources(new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build()).getMetadataBuilder().build().buildSessionFactory();
+		
+	
+		
+		//metadata.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.persist(emp);
+		tx.commit();
+		
+	//	Employee employee = session.get(Employee.class, 8);
+	//	System.out.println(employee);
+		
+	//	Employee employee = session.find(Employee.class, 3);
+	//	System.out.println(employee);
+		
+		//Employee employee = new Employee();
+	//	session.load(employee, 5);
+	//	System.out.println(employee);
+	}
+}
