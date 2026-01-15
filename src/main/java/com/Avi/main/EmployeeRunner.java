0e99c4f169206a5b9bd7d2621a8c0db73344e53b
AvyaGupta-14 <avyagupta7@gmail.com>
2026-01-16 03:21:38 +0530

    update

--- src/main/java/com/Avi/main/EmployeeRunner.java
+++ src/main/java/com/Avi/main/EmployeeRunner.java
@@ -15,7 +15,11 @@ public class EmployeeRunner {
 	public static void main(String[] args) {
 		
 		
+<<<<<<< Updated upstream
 		Employee emp = new Employee(8, "Renu", "Female", 64548);
+=======
+		Employee emp = new Employee(9, "Ritu", "Female", 64548, "India");
+>>>>>>> Stashed changes
 	//	Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
 	//	SessionFactory sessionFactory = cfg.buildSessionFactory();
 	//	SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
