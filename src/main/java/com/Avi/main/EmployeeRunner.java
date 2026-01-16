796766614b96513fe5bfff82c028f2d364b12505
AvyaGupta-14 <avyagupta7@gmail.com>
2026-01-16 15:21:17 +0530

    update

--- src/main/java/com/Avi/main/EmployeeRunner.java
+++ src/main/java/com/Avi/main/EmployeeRunner.java
@@ -1,5 +1,7 @@
 package com.Avi.main;
 
+import java.util.List;
+
 import org.hibernate.Session;
 //import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -18,10 +20,19 @@ public class EmployeeRunner {
 	public static void main(String[] args) {
 		
 		
+<<<<<<< Updated upstream
 		Employee emp1 = new Employee(10, "Shivam", "Male", 64548, "India");
 		Employee emp2 = new Employee(11, "Vimla", "Female", 64548, "India");
 		Employee emp3 = new Employee(12, "Keshav", "Female", 64548, "India");
 		Employee emp4 = new Employee(13, "Kriti", "Female", 64548, "India");
+=======
+		Employee emp1 = new Employee(10, "Avya", "Female", 64548, "India");
+		Employee emp2 = new Employee(11, "Nitin", "Female", 64548, "India");
+		Employee emp3 = new Employee(12, "Neelam", "Female", 64548, "India");
+		Employee emp4 = new Employee(12, "Jack", "Male", 64548, "India");
+		Employee emp5 = new Employee(12, "Atharv", "Male", 64548, "India");
+		Employee emp6 = new Employee(12, "Naitik", "Male", 64548, "India");
+>>>>>>> Stashed changes
 
 		
 	//	Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
@@ -45,11 +56,44 @@ public class EmployeeRunner {
 //		session.persist(emp2);
 //		session.persist(emp3);
 //		session.persist(emp4);
+<<<<<<< Updated upstream
 //		tx.commit();
 		
 
 		Query<Employee> query = session.createQuery("from empp",Employee.class);
 		System.out.println(query.list());
+=======
+//		session.persist(emp5);
+//		session.persist(emp6);
+	//	tx.commit();
+		
+		//Example of hql to get all the records
+	//	Query query1 = session.createQuery("from empp",Employee.class);
+//		List list = query1.list();
+//		System.out.println(list);
+		
+		
+		//HQL to get record with pagination
+//		query1.setFirstResult(2);
+//		query1.setMaxResults(5);
+//		System.out.println(query1.list());
+		
+//		Query q = session.createQuery("select max(name) from empp");
+//		List list1=q.list();
+//		System.out.println(list1);
+		
+		
+		//HQL update query using named parameters
+	//	Query q = session.createQuery("update empp set name=:n where id=:i");
+		session.createMutationQuery("update empp set name=:n where id=:i");
+
+		q.setParameter("n","Pankaj");
+		q.setParameter("i",1);
+		System.out.print("status: "+q.executeUpdate());
+		
+		tx.commit();
+
+>>>>>>> Stashed changes
 	//	Employee employee = session.get(Employee.class, 8);
 	//	System.out.println(employee);
 		
